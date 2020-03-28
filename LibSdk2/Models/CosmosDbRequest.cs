79a3d56f7258101d01c508ea7d3ede2b899e4177
Aymen TROUDI <aymen.troudi@younited-credit.fr>
2020-03-28 21:58:46 +0100

    Add request by id, partition key

--- LibSdk2/Models/CosmosDbRequest.cs
+++ LibSdk2/Models/CosmosDbRequest.cs
@@ -1,4 +1,5 @@
 ﻿using LibSdk2.Settings;
+using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 
 namespace LibSdk2.Models
@@ -8,22 +9,31 @@ namespace LibSdk2.Models
         public string Query { get; }
         public FeedOptions Options { get; }
 
-        public CosmosDbRequest(string query, bool enableCrossPartition)
+        public CosmosDbRequest(string collectionName, string id, string partitionKey)
+        {
+            Query = $@"SELECT * FROM {collectionName} WHERE {collectionName}.id = '{id}'";
+            Options = new FeedOptions
+            {
+                PartitionKey = new PartitionKey(partitionKey)
+            };
+        }
+
+        public CosmosDbRequest(string query, FeedOptions options = null)
         {
             Query = query;
+            Options = options;
+        }
+
+        public CosmosDbRequest(string query, bool enableCrossPartition) : this(query)
+        {
             Options = new FeedOptions
             {
                 EnableCrossPartitionQuery = enableCrossPartition
             };
         }
 
-        public CosmosDbRequest(CosmosDbQuery cosmosDbQuery)
+        public CosmosDbRequest(CosmosDbQuery cosmosDbQuery) : this(cosmosDbQuery.Query, cosmosDbQuery.EnableCrossPartition)
         {
-            Query = cosmosDbQuery.Query;
-            Options = new FeedOptions
-            {
-                EnableCrossPartitionQuery = cosmosDbQuery.EnableCrossPartition
-            };
         }
     }
 }
